Fixed a bug in calculating the error in backpropagation. Changed the calculation of the loop-error in the learning loop. Got the handling of the NMIST data running.

mlp.py
@@ -51,33 +51,23 @@ class MLP:
     def learn(self, m_x, m_y, learning_rate, max_loops=1000):
         loop_nr = 0
         while True:
-            for i in range(len(m_x)):                  # for each example - index: i
+            error = 0.0
+            for ex in range(len(m_x)):                  # for each example - index: ex
                 # Just copy the input into INPUT of layer 0
-                self.layers[0].input = m_x[i]
+                self.layers[0].input = m_x[ex]
 
                 # Now propagate a[0] forward and store the interim results a[l] for the layers l
-                # for l in range(self.num_layers):    # for each layer except of the first - index l
-                #     layer = self.layers[l]
-                #     y = np.empty(layer.num_out)
-                #     res = np.empty(layer.num_out)
-                #     for j in range(layer.num_out):    # for each row in weight matrix of layer l - index j
-                #         res[j] = np.dot(layer.weights[j], layer.input)
-                #         y[j] = self.activation(res[j])
-                #
-                #     layer.sum = res   # = SUM(wv)
-                #     layer.output = y  # = g(RAW_OUT) = g(SUM(wv))
-                #     if l < self.num_layers - 1:
-                #         self.layers[l + 1].input = np.copy(self.layers[l].output)
-
                 for l in range(self.num_layers):
                     self.layers[l].sum = np.dot(self.layers[l].weights, self.layers[l].input)
                     self.layers[l].output = self.activation(self.layers[l].sum)
                     if (l < self.num_layers - 1):
                         self.layers[l + 1].input = self.layers[l].output
+                        # self.layers[l + 1].input = np.copy(self.layers[l].output)
 
                 # Compute the error at the output layer
                 out_layer = self.layers[self.num_layers - 1]  # output layer
-                out_layer.delta = self.deriv_act(out_layer.output) * (m_y[i] - out_layer.output) # i = example index
+                # m_y is not an mdarray! ToDo!
+                out_layer.delta = self.deriv_act(out_layer.sum) * (m_y[ex] - out_layer.output) # ex = example index
 
                 # Compute the other deltas, backwards running through the layers
                 for l in reversed(range(self.num_layers - 1)):
@@ -88,22 +78,24 @@ class MLP:
                     layer_m.delta = delta_m
 
                 # And finally adjust weights.
-                # This may be done in forward order because all data are available here.
+                # This can be done in forward order because all data are available here.
                 for l in range(self.num_layers):
                     layer = self.layers[l]
-                    for j in range(layer.num_out):
-                        delta_w = learning_rate * layer.delta[j] * layer.input
-                        row = layer.weights[j] + delta_w
-                        layer.weights[j] = row
+                    for i in range(layer.num_out):
+                        delta_w = learning_rate * layer.delta[i] * layer.input
+                        row = layer.weights[i] + delta_w
+                        layer.weights[i] = row
                         # layer.weights[j] = layer.weights[j] + delta_w
 
+                # Calculate a more or less reasonable error metric
+                error += Functions.squared_error(m_y[ex], self.layers[self.num_layers - 1].output)
+
             # Calculate a more or less reasonable error metric
-            error = Functions.squared_error(m_y[i], self.layers[self.num_layers - 1].output)
-            error = math.sqrt(error)
+            error /= len(m_x)
             loop_nr += 1
 
-            if error < 0.001 or loop_nr > max_loops:
-                # print("learning loops: ", loop_nr)
+            if error < 1.0E-9 or loop_nr >= max_loops * len(m_x):
+                print("learning loops: ", loop_nr / len(m_x))
                 break  # while
 
         return

program.py
@@ -28,15 +28,15 @@ class Program:
         self.m_x.append(np.array([-1, 1, 1]))
         
         self.m_y.append(np.array([0.1, 0.2]))
-        self.m_y.append(np.array([0.3, 0.4]))
-        self.m_y.append(np.array([0.6, 0.6]))
+        self.m_y.append(np.array([0.3, 0.45]))
+        self.m_y.append(np.array([0.6, 0.55]))
         self.m_y.append(np.array([0.9, 0.8]))
         
         return
 
 
-    def test(self, mlp, learn_rate, num_loops):
-        mlp.show("before training", self.m_x, self.m_y)  # before training
+    def run_small_data(self, mlp, learn_rate, num_loops):
+        # mlp.show("before training", self.m_x, self.m_y)  # before training
         # training
         mlp.learn(
             self.m_x,       # input matrix/vectors
@@ -45,7 +45,7 @@ class Program:
             num_loops   # number of loops
             )
         #
-        mlp.show("after training", self.m_x, self.m_y)  # after training
+        # mlp.show("after training", self.m_x, self.m_y)  # after training
         #
         # Now test an input vector unknown to the net
         arr = np.array([-1, 0.8, 0.7])
@@ -54,7 +54,7 @@ class Program:
         return
 
 
-    def run(self, mlp, m_x, m_y, learn_rate, num_loops):
+    def run_mnist_data(self, mlp, m_x, m_y, learn_rate, num_loops):
         # training
         mlp.learn(
             m_x,        # input matrix/vectors
@@ -77,43 +77,62 @@ class Program:
         plt.show()
 
 
+    def small_data(self):
+        mlp = MLP([3, 2])
+        mlp.init_layers()
+        mlp.set_activation("sigmoid")
+        #
+        self.set_input()
+        #
+        self.run_small_data(mlp, 0.1, 2000)
+        mlp.show("Small Data Test", self.m_x, self.m_y)
+
+
+    def mnist(self):
+        ###--- Load images and labels from MNIST data set ---###
+        max_items = 1
+        num_images, rows, columns, images, raw_images = \
+            Utils.load_mnist_data(Utils.mnist_train_data, max_items=max_items, only_subset=True)
+        num_labels, labels, lbl_vals = \
+            Utils.load_mnist_labels(Utils.mnist_train_labels, max_items=max_items)
+
+        ###--- Print one data set ---###
+        print "num_images=", num_images, " rows=", rows, " columns=", columns
+        # print images[0]
+        print "num_labels=", num_labels
+        print labels[0], " = ", lbl_vals[0]
+        # for i in range(max_items):
+        #     prog.view_image(raw_images[i])
+
+        '''
+        ###--- Train and recognize one data set ---###
+        prog.set_input()
+        input_dim = prog.m_x[0].size
+        print "input_dim=", input_dim
+        mlp = MLP([input_dim, 5, 2])
+        mlp.init_layers()
+        mlp.set_activation("sigmoid")
+        self.run_mnist_data(mlp, prog.m_x, prog.m_y, 5, 1000)
+        mlp.show("Trained Network", prog.m_x, prog.m_y)
+        '''
+
+        ###--- Train and recognize one data set ---###
+        input_dim = images[0].size
+        print "input_dim=", input_dim
+        mlp = MLP([input_dim, 25, 10])
+        mlp.init_layers()
+        mlp.set_activation("sigmoid")
+        self.run_mnist_data(mlp, images, labels, 0.2, 1000)
+        mlp.show("Trained Network", images, labels)
+
+
+
 
 if __name__ == '__main__':
 
     prog = Program()
+    prog.small_data()
 
-    ###--- Load images and labels from MNIST data set ---###
-    max_items = 30
-    num_images, rows, columns, images, raw_images = \
-        Utils.load_mnist_data(Utils.mnist_train_data, max_items=max_items)
-    num_labels, labels, lbl_vals = \
-        Utils.load_mnist_labels(Utils.mnist_train_labels, max_items=max_items)
+    prog.mnist()
 
-    ###--- Print one data set ---###
-    print "num_images=", num_images, " rows=", rows, " columns=", columns
-    # print images[0]
-    print "num_labels=", num_labels
-    print labels[0], " = ", lbl_vals[0]
-    # for i in range(max_items):
-    #     prog.view_image(raw_images[i])
-
-    ###--- Train and recognize one data set ---###
-    prog.set_input()
-    input_dim = prog.m_x[0].size
-    print "input_dim=", input_dim
-    mlp = MLP([input_dim, 5, 2])
-    mlp.init_layers()
-    mlp.set_activation("sigmoid")
-    prog.run(mlp, prog.m_x, prog.m_y, 5, 1000)
-    mlp.show("Trained Network", prog.m_x, prog.m_y)
-
-    #'''
-    ###--- Train and recognize one data set ---###
-    input_dim = images[0].size
-    print "input_dim=", input_dim
-    mlp = MLP([input_dim, 25, 10])
-    mlp.init_layers()
-    mlp.set_activation("sigmoid")
-    prog.run(mlp, images, labels, 0.2, 5000)
-    mlp.show("Trained Network", images, labels)
-    #'''
+    pass

utils.py
@@ -12,21 +12,22 @@ import struct
 
 class Utils:
 
-    mnist_train_data   = "train-images-idx3-ubyte"
-    mnist_train_labels = "train-labels-idx1-ubyte"
-    mnist_test_data    = "t10k-images-idx3-ubyte"
-    mnist_test_labels  = "t10k-labels-idx1-ubyte"
+    mnist_train_data   = "train-images.idx3-ubyte"
+    mnist_train_labels = "train-labels.idx1-ubyte"
+    mnist_test_data    = "t10k-images.idx3-ubyte"
+    mnist_test_labels  = "t10k-labels.idx1-ubyte"
 
 
     @classmethod
-    def load_mnist_data(cls, file_name, max_items=-1):
-        f = open('../data/' + file_name, 'rb')  # do not forget the 'b' for binary read!
-        magic = f.read(4)  # ignore
+    def load_mnist_data(cls, file_name, max_items=-1, only_subset=False):
+        f = open('./data/' + file_name, 'rb')  # do not forget the 'b' for binary read!
+        magic = f.read(4)  # ignore the "magic" number...
         num_items = struct.unpack('>i', f.read(4))[0]
         if max_items >= 0:
             num_items = max_items
         rows = struct.unpack('>i', f.read(4))[0]
         columns = struct.unpack('>i', f.read(4))[0]
+
         img_size = rows * columns
         #
         data = np.fromfile(f, dtype=np.uint8, count=num_items*img_size)
@@ -40,7 +41,11 @@ class Utils:
         for d in data:
             img_vec = np.zeros((img_size + 1,), dtype=float)    # +1 for the mandatory -1
             img_vec[0] = -1.0
-            img_vec[1:] = d
+            # Option to make the data much smaller for testing purposes
+            if only_subset:
+                img_vec[1:] = d[14]  # take only row 14 - it should hold some pixel set
+            else:
+                img_vec[1:] = d
             images.append(img_vec)
         #
         return [num_items, rows, columns, images, data]
@@ -48,7 +53,7 @@ class Utils:
 
     @classmethod
     def load_mnist_labels(cls, file_name, max_items=-1):
-        f = open('../data/' + file_name, 'rb')
+        f = open('./data/' + file_name, 'rb')
         magic = f.read(4)  # ignore
         num_items = struct.unpack('>i', f.read(4))[0]
         if max_items >= 0: